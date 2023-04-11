530bac059ff36aa17aedf0fb929685701806ee17
Andres <salamancaclaudioandres@gmail.com>
2023-04-11 17:08:45 -0300

    update

--- terraform/modules/api_gateway/variables.tf
+++
@@ -1,5 +0,0 @@
-#variable "rest_api_stage_name" {
-#  type        = string
-#  description = "The name of the API Gateway stage"
-#  default     = "dev" //add a stage name as per your requirement
-#}
